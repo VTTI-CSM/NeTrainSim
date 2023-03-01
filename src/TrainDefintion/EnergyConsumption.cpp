0f3005eaca9810ab0872989ad57bb55724ed8780
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-02-28 21:07:21 -0500

    use efficiency equations instead of mapping matrix

--- src/TrainDefintion/EnergyConsumption.cpp
+++ src/TrainDefintion/EnergyConsumption.cpp
@@ -1,149 +1,31 @@
 #include "TrainTypes.h"
 #include "EnergyConsumption.h"
-#include "../util/Utils.h"
-#include "../dependencies/tinyxml2/tinyxml2.h"
+#include <cmath>
 
-
-
-using namespace tinyxml2;
-
-//std::string EC::getEfficiencyValue(const std::string& tagname, const std::vector<std::string>& attrname, 
-//    const std::vector<std::string>& attrvalue) {
-//    XMLDocument doc;
-//    if (doc.LoadFile(EC::DefaultECFileName) != XML_SUCCESS)
-//    {
-//        std::cerr << "Error loading file: " << EC::DefaultECFileName << std::endl;
-//        return "";
-//    }
-//
-//    XMLElement* root = doc.RootElement();
-//    if (!root)
-//    {
-//        std::cerr << "No root element found." << std::endl;
-//        return "";
-//    }
-//
-//    for (XMLElement* elem = root->FirstChildElement(tagname.c_str()); elem != nullptr; elem = elem->NextSiblingElement(tagname.c_str()))
-//    {
-//        bool all_conditions_met = true;
-//        for (size_t i = 0; i < attrname.size(); i++)
-//        {
-//            const char* attr = elem->Attribute(attrname[i].c_str());
-//            if (!attr || std::string(attr) != attrvalue[i])
-//            {
-//                all_conditions_met = false;
-//                break;
-//            }
-//        }
-//
-//        if (all_conditions_met)
-//        {
-//            const char* value = elem->Attribute("values");
-//            if (value)
-//            {
-//                return std::string(value);
-//            }
-//        }
-//    }
-//
-//    std::cerr << "No matching entry found." << std::endl;
-//    return "";
-//}
 
 namespace EC {
-    Map<TrainTypes::PowerType, Vector<double>> locomotiveBusToTankEff;
-    Map<TrainTypes::PowerType, Vector<double>> locomotiveWheelToDCBusEff;
 
+    double EC::getDriveLineEff(double trainSpeed, int notchNumberIndex, TrainTypes::PowerType powerType) {
+        double u_kmh = trainSpeed * 3.6;
+        double wheelToDCBusEff = (u_kmh < 33.0) ? 0.2 + 0.018182 * u_kmh : (u_kmh < 44.0) ? 0.8 + 0.009091 * (u_kmh - 33.0) : 0.9;
 
-    // function to get all attributes and values of a specific tag in an XML file
-    std::vector<std::map<std::string, std::string>> EC::readXML(const char fileName[], const std::string& tagname) {
-        XMLDocument doc;
-        std::vector<std::map<std::string, std::string>> result;
-
-        if (doc.LoadFile(fileName) != XML_SUCCESS)
-        {
-            throw std::runtime_error("Error loading Energy Consumption Database!");
-            exit(1);
+        double DCBusToTank = 0.0;
+        if (powerType == TrainTypes::PowerType::diesel){
+            DCBusToTank = 0.2939 * std::pow(notchNumberIndex,0.2075);
         }
-        XMLElement* root = doc.RootElement();
-        if (!root)
-        {
-            throw std::runtime_error("No root element found!");
-            exit(1);
+        else if (powerType == TrainTypes::PowerType::dieselElectric){
+            DCBusToTank = 0.2939 * std::pow(notchNumberIndex,0.2075);
         }
-
-
-        for (tinyxml2::XMLElement* elem = root->FirstChildElement(tagname.c_str());
-            elem != NULL; elem = elem->NextSiblingElement(tagname.c_str())) {
-
-            std::map<std::string, std::string> elemMap;
-
-            for (const tinyxml2::XMLAttribute* attr = elem->FirstAttribute(); attr != NULL; attr = attr->Next()) {
-                elemMap[attr->Name()] = attr->Value();
-            }
-
-            //elemMap["value"] = elem->GetText();
-
-            result.push_back(elemMap);
+        else if (powerType == TrainTypes::PowerType::electric){
+            DCBusToTank = 0.01 * notchNumberIndex + 0.93;
         }
-
-        return result;
-
-        //TrainTypes::strToPowerType(dd)
-    }
-
-    void EC::extractEnergyMapping(std::vector<std::map<std::string, std::string>> data) {
-        for (auto& elementMap : data) {
-            if (elementMap["type"] == "Wheel To Bus") {
-                if (elementMap["vehicle"] == "locomotive") {
-                    TrainTypes::PowerType locType = TrainTypes::strToPowerType(elementMap["motor"]);
-                    auto dd = Utils::splitStringToDoubleVector(elementMap["values"]);
-                    locomotiveWheelToDCBusEff[locType] = dd;
-                }
-                //else if (elementMap["vehicle"] == "RailCar") {
-                //    TrainTypes::CarType carType = TrainTypes::strtoCarType(elementMap["motor"]);
-                //    EC::carWheelToDCBusEff[carType] = Utils::splitStringToDoubleVector(elementMap["values"]);
-                //}
-            }
-            else if (elementMap["type"] == "Bus To Tank") {
-                if (elementMap["vehicle"] == "locomotive") {
-                    TrainTypes::PowerType locType = TrainTypes::strToPowerType(elementMap["motor"]);
-                    locomotiveBusToTankEff[locType] = Utils::splitStringToDoubleVector(elementMap["values"]);
-                }
-                //else if (elementMap["vehicle"] == "RailCar") {
-                //    TrainTypes::CarType carType = TrainTypes::strtoCarType(elementMap["motor"]);
-                //    EC::carBusToTankEff[carType] = Utils::splitStringToDoubleVector(elementMap["values"]);
-                //}
-            }
+        else if (powerType == TrainTypes::PowerType::hydrogen){
+            DCBusToTank = 0.01 * notchNumberIndex + 0.45;
         }
-    }
-
-    void EC::readDefaultValuesFromConfigFile(const char configurationFile[], string tagName)
-    {
-        extractEnergyMapping(EC::readXML(configurationFile, tagName));
-    }
-
-
-
-
-    double EC::getDriveLineEff(int notchNumberIndex, TrainTypes::PowerType powerType) {
-        if (EC::locomotiveWheelToDCBusEff[powerType].empty() ||
-            EC::locomotiveBusToTankEff[powerType].empty()) {
-
-            EC::readDefaultValuesFromConfigFile();
+        else if (powerType == TrainTypes::PowerType::hydrogenHybrid){
+            DCBusToTank = 0.01 * notchNumberIndex + 0.45;
         }
-
-        int busIndx = notchNumberIndex;
-        int wheelIndx = notchNumberIndex;
-        if (notchNumberIndex > (EC::locomotiveBusToTankEff[powerType].size() - 1)) {
-            busIndx = EC::locomotiveBusToTankEff[powerType].size() - 1;
-        }
-        if (notchNumberIndex > (EC::locomotiveWheelToDCBusEff[powerType].size() - 1)) {
-            wheelIndx = EC::locomotiveWheelToDCBusEff[powerType].size() - 1;
-        }
-        double busToTankE = EC::locomotiveBusToTankEff[powerType].at(busIndx);
-        double wheelToDC = EC::locomotiveWheelToDCBusEff[powerType].at(wheelIndx);
-        return busToTankE * wheelToDC;
+        return wheelToDCBusEff * DCBusToTank;
     }
 
 }
--- src/TrainDefintion/EnergyConsumption.h
+++ src/TrainDefintion/EnergyConsumption.h
@@ -9,54 +9,62 @@
 #include <string>
 #include <iostream>
 #include "TrainTypes.h"
-#include "../util/Map.h"
-#include "../util/Vector.h"
+
 
 using namespace std;
 
 namespace EC {
+	/** (Immutable) the default ec file name[] */
 	static const char DefaultECFileName[] = "ECEDB.xml";
-	// these variables will be overwritten by the energyConfiguration file
+	/** These variables will be overwritten by the energyConfiguration file */
 #pragma region LocomotiveECVariables
 	static double DefaultLocomotiveBatteryMaxCharge = 120.0;
+	/** The default locomotive battery initial charge */
 	static double DefaultLocomotiveBatteryInitialCharge = 0.9;
 
+	/** The default locomotive tank maximum capacity */
 	static double DefaultLocomotiveTankMaxCapacity = 1000.0;
+	/** The default locomotive tank initial capacity */
 	static double DefaultLocomotiveTankInitialCapacity = 0.9;
 #pragma endregion
 
+	/** The default car battery maximum capacity */
 #pragma region RailcarECVariables
 	static double DefaultCarBatteryMaxCapacity = 10000.0;
+	/** The default car battery initial charge */
 	static double DefaultCarBatteryInitialCharge = 0.9;
 
+	/** The default car tender maximum capacity */
 	static double DefaultCarTenderMaxCapacity = 1000.0;
+	/** The default car tender initial capacity */
 	static double DefaultCarTenderInitialCapacity = 0.0;
 #pragma endregion
 
+	/** The default diesel conversion factor */
 #pragma region GeneralECVariables
 	static double DefaultDieselConversionFactor = 0.1005;
+	/** The default hydrogen conversion factor */
 	static double DefaultHydrogenConversionFactor = 0.02995;
 
-	static double DefaultDieselDensity = 0.85; //0.85 kg/l   >> should be between 0.82 to 0.85 at 15 degree celsius (average temperature)
+	/** 0.85 kg/l   >> should be between 0.82 to 0.85 at 15 degree celsius (average temperature) */
+	static double DefaultDieselDensity = 0.85;
+	/** The gamma */
 	static double gamma = 0.65;
 #pragma endregion
 
-#pragma region EnergyConsumptionEfficiencies
-	extern Map<TrainTypes::PowerType, Vector<double>> locomotiveBusToTankEff;
-	extern Map<TrainTypes::PowerType, Vector<double>> locomotiveWheelToDCBusEff;
-
-	//static Map<TrainTypes::CarType, Vector<double>> carBusToTankEff;
-	//static Map<TrainTypes::CarType, Vector<double>> carWheelToDCBusEff;
-#pragma endregion
-
-	void readDefaultValuesFromConfigFile(const char configurationFile[] = EC::DefaultECFileName, string tagName = "Efficiency");
-	std::vector<std::map<std::string, std::string>> readXML(const char fileName[], const std::string& tagname);
-	void extractEnergyMapping(std::vector<std::map<std::string, std::string>> data);
-	//std::string getEfficiencyValue(const std::string& tagname, const std::vector<std::string>& attrname, 
-	//	const std::vector<std::string>& attrvalue);
-
-	//TODO: read the driveline eff from the xml file
-	double getDriveLineEff(int notchNumberIndex, TrainTypes::PowerType powerType);
+	/**
+	 * Gets drive line eff
+	 *
+	 * @author	Ahmed Aredah
+	 * @date	2/28/2023
+	 *
+	 * @param 	trainSpeed			The train speed.
+	 * @param 	notchNumberIndex	Zero-based index of the notch number.
+	 * @param 	powerType			Type of the power.
+	 *
+	 * @returns	The drive line eff.
+	 */
+	double getDriveLineEff(double trainSpeed, int notchNumberIndex, TrainTypes::PowerType powerType);
 
 }
 
