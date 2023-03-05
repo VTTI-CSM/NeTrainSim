3ef3a2de45b867102fa17b452606262472b7f089
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-03-04 22:07:23 -0500

    clear the vector instead of creating new empty one

--- src/network/NetSignalGroupController.cpp
+++ src/network/NetSignalGroupController.cpp
@@ -41,57 +41,60 @@ void NetSignalGroupController::sendPassRequestToControlTo(std::shared_ptr<NetSig
 		if (simulationTime - this->timeStamp > 5) {
 			this->timeStamp = simulationTime;
 			this->clear();
-			this->movements[networkSignal] = true;
-		}
-	}
+            this->movements[networkSignal] = true;
+        }
+    }
 }
 
 void NetSignalGroupController::setSignalsInSameDirection(Vector<std::shared_ptr<NetSignal>> sameDirectionSignals) {
-	// if none is provided, make all signals in the group as the other direction except the LockSignal
-	if (sameDirectionSignals.empty()) {
-		// all signals are should be in the opposite direction except the LockSignal
-		this->otherDirectionSignals = this->networkSignalsGroup;
-		// remove the lock signal from the other direction signals
-		if (this->lockedOnSignal != nullptr) {
-			if (this->otherDirectionSignals.exist(this->lockedOnSignal)) {
-				this->otherDirectionSignals.removeValue(this->lockedOnSignal);
-			}
-		}
-	}
-	// if other direction signals is None, add all signals in the controller to the other direction
-	// signals if they are not in the same direction
-	if (this->otherDirectionSignals.empty()) {
-		for (auto& netSignal : this->networkSignalsGroup) {
-			if (sameDirectionSignals.exist(netSignal)) {
-				this->otherDirectionSignals.push_back(netSignal);
-			}
-		}
+    // if none is provided, make all signals in the group as the other direction except the LockSignal
+    if (sameDirectionSignals.empty()) {
+        // all signals are should be in the opposite direction except the LockSignal
+        this->otherDirectionSignals = this->networkSignalsGroup;
+        // remove the lock signal from the other direction signals
+        if (this->lockedOnSignal != nullptr) {
+            if (this->otherDirectionSignals.exist(this->lockedOnSignal)) {
+                this->otherDirectionSignals.removeValue(this->lockedOnSignal);
+            }
+        }
+    }
+    // if other direction signals is None, add all signals in the controller to the other direction
+    // signals if they are not in the same direction
+    if (this->otherDirectionSignals.empty()) {
+        for (auto& netSignal : this->networkSignalsGroup) {
+            if (! sameDirectionSignals.exist(netSignal)) {
+                this->otherDirectionSignals.push_back(netSignal);
+            }
+        }
 
-	}
+    }
 }
 
 void NetSignalGroupController::updateTimeStep(double& timeStep) {
-	if (this->lockedOnSignal != nullptr && !this->atNodes.empty()) {
+    if (this->lockedOnSignal != nullptr && this->atNodes.size() > 1) {
 		this->timeStamp = timeStep;
 	}
 }
 
 std::pair<std::shared_ptr<NetSignal>, Vector<std::shared_ptr<NetSignal>>> NetSignalGroupController::getFeedback() {
+
 	std::shared_ptr<NetSignal> signalOn = std::shared_ptr<NetSignal>();
 	if (this->lockedOnSignal != nullptr) {
-		signalOn = this->lockedOnSignal;
+        signalOn = this->lockedOnSignal;
 	}
-	else {
-		for (auto& netSignal : this->movements.get_keys()) {
-			if (this->movements[netSignal] == true) {
-				this->lockedOnSignal = netSignal;
-				signalOn = this->lockedOnSignal;
-			}
-		}
-	}
-	if (signalOn != nullptr) {
-		return std::make_pair(signalOn, this->otherDirectionSignals);
-	}
+    else {
+        for (auto& netSignal : this->movements.get_keys()) {
+            if (this->movements[netSignal] == true) {
+                this->lockedOnSignal = netSignal;
+                signalOn = this->lockedOnSignal;
+                break;
+            }
+        }
+    }
+    if (signalOn != nullptr) {
+        return std::make_pair(signalOn, this->otherDirectionSignals);
+    }
+    return std::make_pair(signalOn, Vector<std::shared_ptr<NetSignal>>());
 }
 
 
@@ -101,5 +104,5 @@ void NetSignalGroupController::clear() {
 		this->movements[netSignal] = false;
 	}
 	this->lockedOnSignal = nullptr;
-	this->otherDirectionSignals = Vector<std::shared_ptr<NetSignal>>();
+    this->otherDirectionSignals.clear();
 }
