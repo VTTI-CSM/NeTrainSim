d9d5ce512f9b44f568f00762d36c21c6e7e8ab0b
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-03-08 15:47:34 -0500

    set default auxiliary power to zero

--- src/TrainDefintion/EnergyConsumption.h
+++ src/TrainDefintion/EnergyConsumption.h
@@ -17,13 +17,18 @@ namespace EC {
 // ##################################################################
 // #             start: define locomotive default values            #
 // ##################################################################
-    static double DefaultLocomotiveBatteryMaxCharge = 126000.0;
+    /** (Immutable) the default locomotive auxiliary power */
+    static constexpr double DefaultLocomotiveAuxiliaryPower = 0.0;
+    /** The default locomotive battery maximum charge in kWh.
+     Battery capacity of up to 2.4 megawatt hours. */
+    static double DefaultLocomotiveBatteryMaxCharge = 240000.0;
 	/** The default locomotive battery initial charge */
 	static double DefaultLocomotiveBatteryInitialCharge = 0.9;
 
-	/** The default locomotive tank maximum capacity */
-    static double DefaultLocomotiveTankMaxCapacity = 10000.0;
-	/** The default locomotive tank initial capacity */
+    /** The default locomotive tank maximum capacity in Gallons.
+     Tank capacity of 5,300 gallons. */
+    static double DefaultLocomotiveTankMaxCapacity = 20065.0;
+    /** The default locomotive tank initial capacity in Gallons */
 	static double DefaultLocomotiveTankInitialCapacity = 0.9;
 // ##################################################################
 // #             end: define locomotive default values            #
@@ -32,15 +37,19 @@ namespace EC {
 // ##################################################################
 // #                start: define cars default values               #
 // ##################################################################
-	/** The default car battery maximum capacity */
-    static double DefaultCarBatteryMaxCapacity = 126000.0;
+    /** (Immutable) the default car auxiliary power */
+    static constexpr double DefaultCarAuxiliaryPower = 0.0;
+    /** The default car battery maximum capacity in kWh.
+     Tender car could carry 5.1–6.2MWh*/
+    static double DefaultCarBatteryMaxCapacity = 510000.0;
 	/** The default car battery initial charge */
 	static double DefaultCarBatteryInitialCharge = 0.9;
 
-	/** The default car tender maximum capacity */
-    static double DefaultCarTenderMaxCapacity = 10000.0;
+    /** The default car tender maximum capacity in litters.
+     a tender has a capacity of 23000 Gallons. */
+    static double DefaultCarTenderMaxCapacity = 87064.471;
 	/** The default car tender initial capacity */
-	static double DefaultCarTenderInitialCapacity = 0.0;
+    static double DefaultCarTenderInitialCapacity = 0.9;
 // ##################################################################
 // #                 end: define cars default values                #
 // ##################################################################
@@ -48,13 +57,26 @@ namespace EC {
 // ##################################################################
 // #        start: general energy consumption default values        #
 // ##################################################################
-    /** the default diesel conversion factor */
+    /** the default diesel conversion factor.
+     Diesel (Gallons) = Energy Consumption (KW.h) * 0.028571
+     Diesel (Litters) = Energy Consumption (KW.h) * 0.1005 */
 	static double DefaultDieselConversionFactor = 0.1005;
-	/** The default hydrogen conversion factor */
-    static double DefaultHydrogenConversionFactor = 0.00002995;
+    /** the default biodiesel conversion factor.
+     Diesel (Gallons) = Energy Consumption (KW.h) * 0.028571
+     Diesel (Litters) = Energy Consumption (KW.h) * 0.1005 */
+    static double DefaultBiodieselConversionFactor = 0.1005;
+    /** The default hydrogen conversion factor.
+     Under ambient conditions, 1 litter of hydrogen = 0.003 kWh
+     Hydrogen (Litters) = EnergyConsumption (kW.h) * 0.002995*/
+    static double DefaultHydrogenConversionFactor = 0.002995;
 
 	/** 0.85 kg/l   >> should be between 0.82 to 0.85 at 15 degree celsius (average temperature) */
     static double DefaultDieselDensity = 0.00085;
+
+    /** 0.85 kg/l   >> should be between 0.84 to 0.88 at 15 degree celsius (average temperature) */
+    static double DefaultBioDieselDensity = 0.00088;
+    /** 0.071 kg/l */
+    static double DefaultHydrogenDensity = 0.000071;
 	/** The gamma */
 	static double gamma = 0.65;
 // ##################################################################
@@ -73,7 +95,9 @@ namespace EC {
 	 *
 	 * @returns	The drive line eff.
 	 */
-    double getDriveLineEff(double trainSpeed, int notchNumberIndex, TrainTypes::PowerType powerType);
+    double getDriveLineEff(double &trainSpeed, int notchNumberIndex, TrainTypes::PowerType powerType);
+
+    double getGeneratorEff(TrainTypes::PowerType powerType);
 
 }
 
