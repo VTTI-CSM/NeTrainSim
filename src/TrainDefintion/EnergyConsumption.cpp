5b32052c4f58f357587c68dafbf4c3f42ee80c67
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-03-28 12:53:54 -0400

    change eff mapping of hybrid tech to electric eff

--- src/TrainDefintion/EnergyConsumption.cpp
+++ src/TrainDefintion/EnergyConsumption.cpp
@@ -1,69 +1,70 @@
 #include "TrainTypes.h"
 #include "EnergyConsumption.h"
 #include <cmath>
-#include "../util/Utils.h"
 
 
 namespace EC {
 
-    double getDriveLineEff(double &trainSpeed, int notchNumberIndex, double powerAtWheelProportion, TrainTypes::PowerType powerType) {
-        double wheelToDCBussEff = 0.0;
-        double speed = trainSpeed * 3.6;
+    double getDriveLineEff(double &trainSpeed, int notchNumberIndex,
+                           double powerAtWheelProportion,
+                           TrainTypes::PowerType powerType) {
+        double wheelToDCBusEff = 0.0;     // initialize the variable
+        double speed = trainSpeed * 3.6;  // convert the m/s speed to km/h
+        // get the wheel to DC Bus effeciency
+        // check which range the speed is in
         if (speed <= 58.2) {
-            wheelToDCBussEff = 0.2 + 0.0261*speed - 0.0003 * std::pow(speed, (double)2.0) + 0.000001 * std::pow(speed, (double)3.0);
+            wheelToDCBusEff = 0.2 + 0.0261*speed - 0.0003 *
+                    std::pow(speed, (double)2.0) +
+                    0.000001 * std::pow(speed, (double)3.0);
         }
         else {
-            wheelToDCBussEff = 0.9;
+            wheelToDCBusEff = 0.9;  // constant efficiency
         }
 
         double DCBusToTank = 0.0;
         double notchN = (double) notchNumberIndex;
         switch (powerType) {
-            // for all diesel similar generators, use the same eff
+            // for all diesel similar motor, use the same eff
         case TrainTypes::PowerType::diesel:
         case TrainTypes::PowerType::biodiesel:
         case TrainTypes::PowerType::dieselElectric:
             DCBusToTank = -0.0021 * std::pow(notchN, (double)2) + 0.0407 * notchN + 0.2618;
             break;
-            // for electric, use an average value of 0.965
+        // for electric similar motor, use an average value of 0.965
         case TrainTypes::PowerType::electric:
+        case TrainTypes::PowerType::dieselHybrid:
+        case TrainTypes::PowerType::biodieselHybrid:
             DCBusToTank = 0.965;
             break;
-        case TrainTypes::PowerType::dieselHybrid:
-            DCBusToTank = 0.2939 * std::pow(notchN,(double)0.2075);
-            break;
         case TrainTypes::PowerType::hydrogenHybrid:
-            DCBusToTank = -0.09 * std::pow(powerAtWheelProportion, (double)2.0) - 0.0026 * powerAtWheelProportion + 0.5621;
-            break;
-        case TrainTypes::PowerType::biodieselHybrid:
-            DCBusToTank = -0.0045 * std::pow(notchN, (double)2.0) + 0.0649 * notchN + 0.21;
+            DCBusToTank = -0.09 * std::pow(powerAtWheelProportion, (double)2.0) -
+                    0.0026 * powerAtWheelProportion + 0.5621;
             break;
         default:
             break;
         }
-        return wheelToDCBussEff * DCBusToTank;
+        return wheelToDCBusEff * DCBusToTank;
     }
 
     double getGeneratorEff(TrainTypes::PowerType powerType) {
         // TODO: change it to new values
         switch (powerType) {
         case TrainTypes::PowerType::dieselHybrid:
-            return 0.4524;
         case TrainTypes::PowerType::biodieselHybrid:
-            return 0.4524;
+            return 0.4524; // the full throttle eff
         case TrainTypes::PowerType::hydrogenHybrid:
-            return 0.53;
+            return 0.53;  // the full throttle eff
        default:
-            return 1.0;
+            return 1.0;  // if powertype should not generate
         }
     }
 
     double getRequiredGeneratorPowerForRecharge(double batterySOC) {
         // get the battery SOC index 
         int ind = min(static_cast<int>(ceil(batterySOC * 10.0)) , 8);
-        // create a map by the soc index
-        double map[] = {1,1,0.8,0.6,0.4,0.2,0.1,0.0};
-        return map[ind];
+        // find the appropiate required power percentage
+        // by index
+        return requiredGeneratorPower[ind];
     }
 
     double getEmissions(double fuelConsumption) {
